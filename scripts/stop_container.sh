4b198385bbab24682b31589f07b9c704e591a1ed
srihari-2908 <158026866+srihari-2908@users.noreply.github.com>
2024-06-23 23:55:46 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,3 +2,11 @@
 set -e
 
 echo "Hello World"
+count=`docker ps|grep python|wc -l`
+
+if [ $count -gt 0 ];then
+ID=`docker ps|grep python|awk -F " " '{print $1}'`
+docker stop $ID
+else
+echo "No Container are running"
+fi
